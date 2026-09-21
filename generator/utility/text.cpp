61c136b7312f873176c6698673828aeeac57720f
Noaseto <noix.50@hotmail.fr>
2026-09-21 18:44:52 +0200

    a commit to force action builds

--- generator/utility/text.cpp
+++ generator/utility/text.cpp
@@ -857,7 +857,7 @@ const auto kSilverMessageCode = text_color_code(0xBFBFBFFF);
                 japanese += " and " + text.mEntries[Text::JAPANESE].str;
             } else if (i == texts.size() - 1) {
                 english += ", and " + text.mEntries[Text::ENGLISH].str;
-                french += ", and " + text.mEntries[Text::FRENCH].str;
+                french += ", et " + text.mEntries[Text::FRENCH].str;
                 german += ", und " + text.mEntries[Text::GERMAN].str;
                 italian += ", and " + text.mEntries[Text::ITALIAN].str;
                 spanish += ", and " + text.mEntries[Text::SPANISH].str;

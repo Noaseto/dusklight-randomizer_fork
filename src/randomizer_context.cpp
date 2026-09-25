a4d3ce2b54740419401eebccdbfe0330b0501c4e
gymnast86 <stock.joann@gmail.com>
2026-09-24 18:04:31 -0700

    couple telma's door to the non-door entrance

--- src/randomizer_context.cpp
+++ src/randomizer_context.cpp
@@ -1746,7 +1746,7 @@ RandomizerContext WriteSeedData(randomizer::logic::world::World* world) {
             randoData.mEntranceOverrides[forward] = replaces;
 
             // Set overrides for all coupled entrances
-            for (const auto& point : entrance->GetCoupledEntrances()) {
+            for (const auto& point : entrance->GetCoupledPoints()) {
                 RandomizerContext::EntranceOverride coupled = {.stageId = entrance->GetStageId(), .roomNo = entrance->GetRoomNo(), .mapLayer = entrance->GetLayerNo(), .pointNo = point};
                 randoData.mEntranceOverrides[coupled] = replaces;
             }
